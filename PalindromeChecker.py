514f1baecfc3b8ec9084f817ff65d145e7fe9615
Pranjal Sinha <pranjalsinha1965@gmail.com>
2024-08-01 10:58:32 +0530

    oops/python

--- PalindromeChecker.py
+++ PalindromeChecker.py
@@ -1,44 +1,19 @@
-# Function to check if a
-# given integer is a palindrome
-def palindrome(n):
-    # Initialize a variable to
-    # store the reverse of the number
-    revNum = 0
-    # Create a duplicate variable to
-    # store the original number
-    dup = n
-    # Iterate through each digit of
-    # the number until it becomes 0
+def is_palindrome(n: int) -> bool:
+    rev_num = 0
+    original_num = n
     while n > 0:
-        # Extract the last
-        # digit of the number
-        ld = n % 10
-        # Build the reverse number
-        # by appending the last digit
-        revNum = (revNum * 10) + ld
-        # Remove the last digit
-        # from the original number
+        last_digit = n % 10
+        rev_num = (rev_num * 10) + last_digit
         n = n // 10
-    # Check if the original number
-    # is equal to its reverse
-    if dup == revNum:
-        # If equal, return True
-        # indicating it's a palindrome
-        return True
-    else:
-        # If not equal, return False
-        # indicating it's not a palindrome
-        return False
-
+    return original_num == rev_num
 
 def main():
     number = 4554
 
-    if palindrome(number):
-        print(number, "is a palindrome.")
+    if is_palindrome(number):
+        print(f"{number} is a palindrome.")
     else:
-        print(number, "is not a palindrome.")
-
+        print(f"{number} is not a palindrome.")
 
 if __name__ == "__main__":
     main()
