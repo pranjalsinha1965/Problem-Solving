431909ee1c1f5187a304276b83341c0f689d32a2
Pranjal Sinha <pranjalsinha1965@gmail.com>
2024-08-02 15:55:34 +0530

    problem-solving

---
+++ Practice/Roman-Integer.py
@@ -0,0 +1,39 @@
+class Solution:
+    def __init__(self):
+        self.answer = 0
+
+    def romanToInt(self, s: str) -> int:
+        chars = s
+
+        def add(number):
+            self.answer += number
+
+        # inelegantly replace all special cases with simplified versions. IV (4) becomes IIII (1,1,1,1) etc
+        chars = (
+            chars.replace("IV", "IIII")
+            .replace("IX", "VIIII")
+            .replace("XL", "XXXX")
+            .replace("XC", "LXXXX")
+            .replace("CD", "CCCC")
+            .replace("CM", "DCCCC")
+        )
+
+        # basically a switch statement to map each possible roam-nc numeral to an integer value and add it up
+        for char in chars:
+            match char:
+                case "I":
+                    add(1)
+                case "V":
+                    add(5)
+                case "X":
+                    add(10)
+                case "L":
+                    add(50)
+                case "C":
+                    add(100)
+                case "D":
+                    add(500)
+                case "M":
+                    add(1000)
+
+        return self.answer
