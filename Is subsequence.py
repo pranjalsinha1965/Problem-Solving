0ed37f7c1bc55978730c7b0d76e2093a668c44a0
Pranjal Sinha <pranjalsinha1965@gmail.com>
2024-08-13 16:02:48 +0530

    codes/oopS

--- Is subsequence.py
+++ Is subsequence.py
@@ -1,13 +1,19 @@
 class Solution:
-    def isSubsequence(self, s: str, t: str):
+    def isSubsequence(self, s: str, t: str) -> bool:
         if s == "" or s == t: 
             return True
         
-        j=0
+        j = 0
         for i in range(len(t)):
-            if t[i]  == s[j]:
+            if t[i] == s[j]:
                 j += 1
                 if j == len(s):
                     return True
         return False
 
+# Create an instance of the Solution class
+solution = Solution()
+
+# Check if "ababc" is a subsequence of "cabb"
+s = solution.isSubsequence("ababc", "cabb")
+print(s)  # Output: False
