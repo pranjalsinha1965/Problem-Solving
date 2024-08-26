8d6891716a464f6c30c45d74ece4d17bd73c5e14
Pranjal Sinha <pranjalsinha1965@gmail.com>
2024-08-26 20:17:18 +0530

    dry-run

--- rough.py
+++ rough.py
@@ -5,15 +5,15 @@ class Circle:
     def __init__(self, radius=1):
         self.radius = radius
         self.area = radius * radius * Circle.pi
-    
+
     def setRadius(self, new_radius):
         self.radius = new_radius
-        self.area = new_radius * new_radius * self.pi
-    
+        self.area = new_radius ** 2 * self.pi
+
     def getCircumference(self):
         return self.radius * self.pi * 2
     
 c = Circle()
 print('Radius is: ', c.radius)
 print('Area is: ', c.area)
-print('Circumference is:', c.getCircumference())
+print('Circumference is: ', c.getCircumference)
