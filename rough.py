57a4273c4aadba4802f1a4e51dddbeafa748feaa
Pranjal Sinha <pranjalsinha1965@gmail.com>
2024-09-01 20:07:52 +0530

    Advanced / dry / runs

--- rough.py
+++ rough.py
@@ -1,4 +1,17 @@
-from functools import reduce
+import math 
+class Cylinder:
+    def __init__(self, height=1, radius=2):
+        self.height = height
+        self.radius = radius 
 
-reduce(lambda x, y: x if x < y else y, b)
-‘,’.join(list)
+    def volume(self):
+        return math.pi * self.radius ** 2 * self.height 
+    
+    def surface_area(self):
+        return 2 * math.pi * self.radius * self.height + 2 * math.pi * self.radius ** 2
+
+c = Cylinder(2, 3)
+print("Volume: ", c.volume())
+print("Surface Area:", c.surface_area) 
+
+
