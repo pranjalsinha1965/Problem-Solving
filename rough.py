8022fcb88d40184701be402b3ed5aaf41ba2eea1
Pranjal Sinha <pranjalsinha1965@gmail.com>
2024-09-02 22:33:10 +0530

    dry-run/advanced

--- rough.py
+++ rough.py
@@ -1,17 +1,5 @@
-import math 
-class Cylinder:
-    def __init__(self, height=1, radius=2):
-        self.height = height
-        self.radius = radius 
+def reverse(String):
+    return String[::-1]
 
-    def volume(self):
-        return math.pi * self.radius ** 2 * self.height 
-    
-    def surface_area(self):
-        return 2 * math.pi * self.radius * self.height + 2 * math.pi * self.radius ** 2
-
-c = Cylinder(2, 3)
-print("Volume: ", c.volume())
-print("Surface Area:", c.surface_area) 
-
-
+s = "Pranjal"
+print(reverse(s))
