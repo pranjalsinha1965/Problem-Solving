2e9561e4169ef383424dc6e41a58d154c8634329
Pranjal Sinha <pranjalsinha1965@gmail.com>
2024-09-20 04:44:21 +0530

    dry-run

--- rough.cpp
+++ rough.cpp
@@ -29,3 +29,23 @@ int main()
     int val = search(arr, n, num);
     printf("%d", val);
 }
+vector<int> moveZeros(int n, vector<int> a)
+{
+    vector<int> temp;
+    for(int i=0; i<n; i++)
+    {
+        if(a[i] != 0)
+        temp.push_back(a[i]);
+    }
+    int nz = temp.size();
+    for(int i=0; i<nz; i++)
+    {
+        a[i] = temp[i];
+    }
+    for(int i=nz; i<n; i++)
+    {
+        a[i] = 0;
+    }
+    return a;
+}
+
