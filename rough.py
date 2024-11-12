ce2ed4952f3f0b5dcaab142b343ef4c19497b51f
Pranjal Sinha <pranjalsinha1965@gmail.com>
2024-11-12 11:35:49 +0530

    clean up

--- rough.py
+++ rough.py
@@ -1,16 +1,10 @@
-def isPalindrome(s: str) -> bool: 
-    i, j = 0, len(s) - 1
-    while i<j: 
-        if not s[i].isalnum():
-            i += 1
-        elif not s[j].isalnum(): 
-            j -= 1 
-        elif s[i].lower() != s[j].lower(): 
-            return False 
-        else: 
-            i, j = i + 1, j - 1
-    return True 
+from typing import List 
+def maxProfit(self, prices: List[int]) -> int: 
+    profit = 0
+    buy = prices[0]
+    for p in prices:
+        buy = min(buy, p)
+        profit = max(profit, p-buy)
+    return profit 
 
-sentence = "My name is Pranjal Sinha"
-result = isPalindrome(sentence)
-print(f"{result}")
+
