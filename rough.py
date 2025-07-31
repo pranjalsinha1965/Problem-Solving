ad7309d4452729a91a8f04b0a68bdabe11bad12b
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-07-31 22:56:44 +0530

    Keep adding the questions daily

--- rough.py
+++ rough.py
@@ -1,112 +1,35 @@
-from typing import List
+def find_common_elements(list1, list2): 
+    common_elements = []
+    for item in list1: 
+        if item in list2: 
+            common_elements.append(item)
+    return common_elements
 
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda i: i[0])
-        output = [intervals[0]]
+def bubble_sort(elements): 
+    n = len(elements)
+    for i in range(n - 1): 
+        for j in range(n - i - 1): 
+            if elements[j] > elements[j + 1]: 
+                elements[j], elements[j + 1] = elements[j + 1], elements[j]
 
-        for start, end in intervals[1:]:
-            lastEnd = output[-1][1]
-            if start <= lastEnd:
-                output[-1][1] = max(lastEnd, end)
-            else:
-                output.append([start, end])
+def find_second_largest(numbers): 
+    largest = float('-inf')
+    second_largest = float('-inf')
+    for num in numbers: 
+        if num > largest: 
+            second_largest = largest 
+            largest = num
+        elif num > second_largest and num != largest: 
+            second_largest = num
+    return second_largest 
 
-        return output
-    
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+def remove_duplicates(numbers): 
+    unique_numbers = []
+    for num in numbers: 
+        if num not in unique_numbers: 
+            unique_numbers.append(num)
+    return unique_numbers
 
-class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return head
-        newHead = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return newHead
-    
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                return True
-        return False
-    
-class ListNode: 
-    def __init__(self, val = 0, next = None): 
-        self.val = val 
-        self.next = next 
-
-class Solution: 
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode: 
-        dummy = ListNode(0, head)
-        left = dummy 
-        right = head 
-        
-        while n > 0 and right: 
-            right = right.next 
-            n -= 1 
-        
-        while right: 
-            left = left.next 
-            right = right.next 
-            
-        left.next = left.next.next
-        return dummy.next 
-    
-class Solution: 
-    def lengthOfSubstring(self, s: str) -> int: 
-        charSet = set()
-        l = 0
-        res = 0
-        
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l += 1
-            charSet.add(s[r])
-            res = max(res, r - l + 1)
-        return res 
-    
-class Solution: 
-    def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
-        l = 0
-        maxf = 0
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            maxf = max(maxf, count[s[r]])
-            while (r - l + 1) - maxf > k:
-                count[s[l]] -= 1 
-            l += 1
-        res = max(res, r - l + 1)
-        return res 
-    
-# Palindromic Substrings - Leetcode 647 - Python
-# Done 
-class Solution:
-    def countSubstrings(self, s: str) -> int: 
-        for i in range(len(s)): 
-            res += self.countPali(s, i, i)
-            res += self.countPali(s, i, i + 1)
-        return res 
-        
-    def countPali(self, s, l, r): 
-        res = 0
-        while l >= 0 and r < len(s) and s[l] == s[r]: 
-            res += l
-            l -= 1 
-            r += 1
-        return res 
+nums = [1, 2, 3, 2, 1, 4, 5, 4]
+unique_nums = remove_duplicates(nums)
+print(f"{unique_nums}")
--- rough2.py
+++ rough2.py
@@ -266,3 +266,116 @@ class Solution:
                 goal = i 
         
         return True if goal == 0 else False 
+    
+from typing import List
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda i: i[0])
+        output = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            lastEnd = output[-1][1]
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
+            else:
+                output.append([start, end])
+
+        return output
+    
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return newHead
+    
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+    
+class ListNode: 
+    def __init__(self, val = 0, next = None): 
+        self.val = val 
+        self.next = next 
+
+class Solution: 
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode: 
+        dummy = ListNode(0, head)
+        left = dummy 
+        right = head 
+        
+        while n > 0 and right: 
+            right = right.next 
+            n -= 1 
+        
+        while right: 
+            left = left.next 
+            right = right.next 
+            
+        left.next = left.next.next
+        return dummy.next 
+    
+class Solution: 
+    def lengthOfSubstring(self, s: str) -> int: 
+        charSet = set()
+        l = 0
+        res = 0
+        
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res 
+    
+class Solution: 
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        res = 0
+        l = 0
+        maxf = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+            while (r - l + 1) - maxf > k:
+                count[s[l]] -= 1 
+            l += 1
+        res = max(res, r - l + 1)
+        return res 
+    
+# Palindromic Substrings - Leetcode 647 - Python
+# Done 
+class Solution:
+    def countSubstrings(self, s: str) -> int: 
+        for i in range(len(s)): 
+            res += self.countPali(s, i, i)
+            res += self.countPali(s, i, i + 1)
+        return res 
+        
+    def countPali(self, s, l, r): 
+        res = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]: 
+            res += l
+            l -= 1 
+            r += 1
+        return res 
