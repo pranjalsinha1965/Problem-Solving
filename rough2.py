0d6426862493437c36c9fc17333005df03a2fc8d
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-08-07 21:43:56 +0530

    Please keep practicing section: DSA-Based Algorithms

--- rough2.py
+++ rough2.py
@@ -379,3 +379,35 @@ class Solution:
             l -= 1 
             r += 1
         return res 
+    
+def find_common_elements(list1, list2):
+    common_elements = []
+    for item in list1:
+        if item in list2:
+            common_elements.append(item) 
+    return common_elements 
+
+def bubble_sort(elements):
+    n = len(elements)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if elements[j] > elements[j + 1]:
+                elements[j], elements[j + 1] = elements[j + 1], elements[j]
+
+def find_second_largest(numbers):
+    largest = float('-inf')
+    second_largest = float('-inf')
+    for num in numbers:
+        if num > largest:
+            second_largest = largest
+            largest = num
+        elif num > second_largest and num != largest:
+            second_largest = num
+    return second_largest
+
+def remove_duplicates(numbers):
+    unique_numbers = []
+    for num in numbers:
+        if num not in unique_numbers:
+            unique_numbers.append(num)
+    return unique_numbers
