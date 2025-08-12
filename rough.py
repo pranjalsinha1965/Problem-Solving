63a15732fde352ccd41ebbd9b0363d3d0aa81ae3
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-08-13 03:53:23 +0530

    Keep adding small questions and solve, practice daily

--- rough.py
+++ rough.py
@@ -222,3 +222,20 @@ class Solution:
                 if nums[i] < nums[j]:  
                     LIS[i] = max(LIS[i], 1 + LIS[j])  
         return max(LIS)
+
+from typing import List
+class Solution: 
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]: 
+        intervals.sort(key = lambda i: i[0])
+        output = [intervals[0]]
+        for start, end in intervals[1:]: 
+            lastEnd = output[-1][1]
+            if start <= lastEnd: 
+                output[-1][1] = max(lastEnd, end)
+            else: 
+                output.append([start, end])
+        return output 
+    
+intervals = [[8,10],[1,3],[2,6],[15,18]]
+intervals.sort(key=lambda i: i[0])
+print(intervals)
