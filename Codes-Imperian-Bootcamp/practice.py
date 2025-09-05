a61ed8b52676c55e5810c2e63c7bc2c02c012f63
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-09-05 19:43:35 +0530

    Keep practicing these sheets daily

---
+++ Codes-Imperian-Bootcamp/practice.py
@@ -0,0 +1,81 @@
+import ctypes
+def allocate_memory(value): 
+    ptr = ctypes.c_int(value)
+    return ptr.value 
+
+result = allocate_memory(10)
+print(result)
+
+def modify_value(ptr, value): 
+    ptr.value = value
+    return ptr.value 
+
+ptr = ctypes.c_int(10)
+print(ptr)
+
+def dynamic_array(n): 
+    return [n] * n 
+
+number = 49 
+result = dynamic_array(number)
+print(result) 
+
+import ctypes
+
+def allocate_memory(n): 
+    return (ctypes.c_int * n)()
+
+def modify_value(ptr, n): 
+    for i in range(n): 
+        ptr[i] = i 
+    return ptr 
+
+number = 10 
+result = modify_value(allocate_memory(number), number)
+print([result[i] for i in range(number)])
+
+def fibonacci(n): 
+    if n <= 1: 
+        return n 
+    else: 
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+number = 10 
+result = fibonacci(number)
+print(result)
+
+def power(base, exponent): 
+    if exponent == 0:
+        return 1
+    else: 
+        return base * base(base, exponent - 1)
+
+number = 2
+exponent = 3
+resultant = power(number, exponent)
+print(resultant)
+
+def reverse_array(arr): 
+    if not arr: 
+        return 
+    else: 
+        print(arr[-1])
+        reverse_array(arr[:-1])
+
+numbers = [1, 2, 3, 4, 5]
+reverse_array(numbers)
+
+def permutations(s): 
+    if len(s) == 0:
+        return [""]
+    else: 
+        perms = []
+        for i in range(len(s)): 
+            for perm in permutations(s[:i] + s[i + 1:]): 
+                perms.append(s[i] + perm)
+    return perms 
+
+string = "abc"
+result = permutations(string)
+print(result)
+
