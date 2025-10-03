fadfaf23aa4f31985223638978e0131d5b505516
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-10-03 11:15:51 +0530

    New Questions added for interviews

--- Codes-Imperian-Bootcamp/practice.py
+++ Codes-Imperian-Bootcamp/practice.py
@@ -1,19 +1,8 @@
-# How to attempt questions in an interview ?
-
-# 1. Approach: Problem Solving
-
-# A. Explanation using Flow Chart 
-# B. Give line by line comments for the code 
-# C. Provide space and time complexity for the alternatives as well
-# D. Explain Edge cases / Sample test cases afterwards 
-
-# 1. Rotate Image 
-# 2. Anagrams 
-# 3. Lemonade Change
-# 4. Remove Nth Node 
-# 5. Coin Change 
-
-# note topics from kpmg ctf and study for the challenge 
-
-
-
+class Solution: 
+    def isValid(Self, s: str) -> bool: 
+        stack = []
+        closeToOpen = {
+            ')' : '(', 
+            '}' : '{', 
+            ']' : '['
+        }
