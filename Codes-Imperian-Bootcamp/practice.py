28f9e7d77be13c6e22ab21194bfa76c03b0a93c6
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-10-29 16:28:35 +0530

    Keep solving the questions daily

--- Codes-Imperian-Bootcamp/practice.py
+++ Codes-Imperian-Bootcamp/practice.py
@@ -13,3 +13,20 @@
 # CanJump 
 # isHappy 
 
+def count_frequency(numbers):
+    frequency = {}
+    for num in numbers:
+        if num in frequency: 
+            frequency[num] += 1
+        else: 
+            frequency[num] = 1 
+    return frequency
+
+def is_prime(number):
+    if number < 2: 
+        return False 
+    for i in range(2, int(number**0.5) + 1): 
+        if number % i == 0:
+            return False 
+    return True 
+
