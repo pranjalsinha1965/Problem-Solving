bc24904d93bf8105d0d8fcbb2fea5117567f9b7f
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-12-04 13:19:38 +0530

    Add problems for daily practice

--- Codes-Imperian-Bootcamp/practice.py
+++ Codes-Imperian-Bootcamp/practice.py
@@ -13,5 +13,36 @@
 # CanJump 
 # isHappy 
 
+# Matrix concept is very important practice them from the list only common for job interview only 
+
+# 1. Best time to buy and sell 
+# 2. Binary Search 
+# 3. Happy Number: Using Cycle Detection 
+# 4. Eating fresh lemonade change 
+# 5. container with most water 
+# 6. twoSum 
+# 7. Combination Sum - Backtracking 
+# 8. climbStairs - Solution
+# 9. Coin Change - Greedy ? 
+# 10. Length of Increasing subsequence 
+# 11. wordbreak 
+# 12. combination Sum = Backtracking 
+# 13. House Robber 
+# 14. House Robber II 
+# 15. Decode Ways - Dynamic Programming 
+# 16. Unique Paths - DP
+# 17. Clone Graph - DFS 
+# 18. Merge Intervals
+# 19. Reverse Linked List 
+# 20. Linked List Cycle 
+# 21. Rotate Image - Matrix 
+# 22. Longest Substring Without Repeating Characters 
+# 23. Longest Repeating Character Replacement 
+# 24. Valid Anagram 
+# 25. Gorup Anagrams 
+# 26. Valid Parenthesis 
+# 27. Longest Palindromic Substring 
+# 28. Palindromic Substrings 
+# 29. Encode and Decode Strings 
 
 
